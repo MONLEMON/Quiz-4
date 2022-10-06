339bce788bb9e6dc5cd0be0dc921b47f12759911
Surabordin <bluelimec@gmail.com>
2022-10-06 15:47:54 +0700

    complete

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -8,6 +8,14 @@ export default function userRegisterRoute(req, res) {
 
     //check authentication
     const user = checkToken(req);
+    if (isAdmin) {
+      if (!user || !user.isAdmin) {
+        return res.status(403).json({
+          ok: false,
+          message: "You do not have permission to create admin user",
+        });
+      }
+    }
     //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
 
     //validate body
@@ -24,12 +32,30 @@ export default function userRegisterRoute(req, res) {
 
     //check if username is already in database
     const users = readUsersDB();
+    const foundUser = users.find((x) => x.username === username);
+    if (foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
     //return res.status(400).json({ ok: false, message: "Username is already taken" });
-
+    const newUseradmin = {
+      username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin,
+      money: null,
+    };
+    const newUsernormal = {
+      username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin,
+      money: 0,
+    };
     //create new user and add in db
-
+    if (isAdmin === true) users.push(newUseradmin);
+    if (isAdmin === false) users.push(newUsernormal);
     writeUsersDB(users);
 
     //return response
+    return res.json({ ok: true, username, isAdmin });
   }
 }
